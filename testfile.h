defb3c3e3a9ef93a7a34dfbfd944ccf014887974
Thine-Sho <Thine@Oyusha.loca>
2020-04-21 14:42:49 -0700

    Update: file

--- testfile.h
+++ testfile.h
@@ -5,5 +5,19 @@
 #include <map>
 
 class assigner{
+  private:
 
+  public:
+    void new(){
+        for(auto &x: mymap){
+          if(mymap.find(x.first) != mymap.end() ){
+            vector<int>tmp = mymap[x.first];
+            cout << x.first << ": ";
+            for(auto &i: tmp){
+              cout << i << ", ";
+            }
+            cout << endl;
+          }
+      }
+    }
 };
